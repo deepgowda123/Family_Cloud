093903fa50cfd7ed327f0d63678a09fe32c11e81
Deepika <deepikagowda255@gmail.com>
2025-11-19 15:03:05 +0530

    updated test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,14 @@
-def test_sample():
-    assert True
+import sys
+import os
+
+# Add parent dir to import app.py
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import create_app
+
+def test_home_page():
+    app = create_app()
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
