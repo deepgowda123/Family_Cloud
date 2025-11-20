4c703139f717ff815e2e5301d1b035705a665277
Deepika <deepikagowda255@gmail.com>
2025-11-20 11:18:40 +0530

    Fix SonarQube coverage path

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add parent dir to import app.py
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from app import create_app
 
 def test_home_page():
@@ -11,4 +5,3 @@ def test_home_page():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-
